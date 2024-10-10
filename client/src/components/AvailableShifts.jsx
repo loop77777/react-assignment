5d4922b0efefa0a356d9eb4a6a8adb8db69e1307
Deepanshu Malviya <mdeepanshu143@gmail.com>
2024-10-10 21:45:43 +0530

    Post api not working anymore

--- client/src/components/AvailableShifts.jsx
+++ client/src/components/AvailableShifts.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import { useTheme } from "@mui/material/styles";
 import { Button, List, ListItem, Typography } from "@mui/material";
+import axios from "axios";
+import { API_URL } from "../App";
 
 const AvailableShifts = ({ shifts }) => {
-  const [selectedCity, setSelectedCity] = useState("");
   const theme = useTheme();
 
   // Group shifts by city and date
@@ -27,6 +28,10 @@ const AvailableShifts = ({ shifts }) => {
 
   const groupedShifts = groupShiftsByCityAndDate(shifts);
 
+  // Set initial selected city to the first city in the groupedShifts object
+  const initialCity = Object.keys(groupedShifts)[0] || "";
+  const [selectedCity, setSelectedCity] = useState(initialCity);
+
   // Handle tab change
   const handleTabChange = (event, newValue) => {
     setSelectedCity(newValue);
@@ -39,6 +44,48 @@ const AvailableShifts = ({ shifts }) => {
       minute: "2-digit",
     });
   };
+
+  // Check if the shift timing overlaps with the current time
+  const isShiftOverlapping = (startTime, endTime) => {
+    const now = new Date();
+    return now >= new Date(startTime) && now >= new Date(endTime);
+  };
+
+  // Handle booking a shift
+  const handleBookShift = async (shiftId) => {
+    // Find the shift to book
+    const shiftToBook = shifts.find((shift) => shift.id === shiftId);
+    console.log(shiftToBook)
+    // Check if the shift is already booked
+
+    if (shiftToBook.booked) {
+      // Cancel the booking
+      try {
+        await axios.post(`${API_URL}/shifts/${shiftId}/cancel`);
+        // shiftToBook.booked = false;
+        console.log("shift cancelled");
+      } catch (error) {
+        console.error("Error cancelling the shift:", error);
+      }
+    } else {
+      // Book the shift
+      try {
+        await axios.post(`${API_URL}/shifts/${shiftId}/book`);
+        // shiftToBook.booked = true;
+        console.log("shift booked");
+      } catch (error) {
+        console.error("Error booking the shift:", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Update selected city when shifts data changes
+    if (Object.keys(groupedShifts).length > 0) {
+      setSelectedCity(Object.keys(groupedShifts)[0]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shifts]);
 
   return (
     <div>
@@ -86,13 +133,25 @@ const AvailableShifts = ({ shifts }) => {
                     }}
                   >
                     {formatTime(shift.startTime)} - {formatTime(shift.endTime)}
+                    {shift.booked && (
+                      <span style={{ marginRight: "10px" }}>Booked</span>
+                    )}
                     <Button
+                      onClick={() => handleBookShift(shift.id)}
                       sx={{
-                        bgcolor: theme.typography.allVariants.color.success,
+                        bgcolor: shift.booked
+                          ? theme.typography.allVariants.color.alert
+                          : theme.typography.allVariants.color.success,
                         ml: "auto",
+                        border: 1,
                       }}
+                      disabled={isShiftOverlapping(
+                        shift.startTime,
+                        shift.endTime
+                      )}
+                      variant="outlined"
                     >
-                      Book
+                      {shift.booked ? "Cancel" : "Book"}
                     </Button>
                   </ListItem>
                 ))}
